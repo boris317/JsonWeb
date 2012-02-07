4604a574a76d2d7b604580987cbb76d0786f7ec5
Shawn Adams <shawn@mobiata.com>
2012-02-07 15:24:15 -0500

    - Refactored String, Float and Integer validators to be base classes of   EnsureType - Added new validator Number - a valid value is an int or float. - Added the ability to explicity set the type_name in the EnsureType   intializer.

--- jsonweb/schema.py
+++ jsonweb/schema.py
@@ -28,8 +28,9 @@ class Validator(object):
         return self.validate(item)
         
     def _validate(self, item):
+        
         raise NotImplemented
-    def _type_name(self, obj):
+    def _class_name(self, obj):
         return obj.__class__.__name__
     
 """
@@ -52,7 +53,7 @@ class ObjectSchema(Validator):
         val_obj = {}
         errors = {}        
         if not isinstance(obj, dict):
-            raise ValidationError("Expected dict got %s instead" % (self._type_name(obj)))
+            raise ValidationError("Expected dict got %s instead" % (self._class_name(obj)))
         for field in self._fields:
             v = getattr(self, field)
             try:
@@ -65,13 +66,7 @@ class ObjectSchema(Validator):
         if errors:
             raise ValidationError("Error validating object", errors)
         return val_obj
-    
-    def _prop_str(self, k=None):
-        cls = self.__class__.__name__
-        if k:
-            return "%s.%s" % (cls, k)
-        return cls 
-                
+                    
 class List(Validator):
     def __init__(self, validator, **kw):
         Validator.__init__(self, **kw)
@@ -79,7 +74,7 @@ class List(Validator):
         
     def validate(self, item):
         if not isinstance(item, list):
-            raise ValidationError("Expected list got %s instead" % self._type_name(item))
+            raise ValidationError("Expected list got %s instead" % self._class_name(item))
         validated_objs = []
         errors = []
 
@@ -97,48 +92,55 @@ class List(Validator):
 """
 Simple Validators
 """
-    
-            
+     
 class EnsureType(Validator):
-    def __init__(self, _type):
-        self._type = _type
+    def __init__(self, _type, type_name=None, **kw):
+        super(EnsureType, self).__init__(**kw)
+        self.__type = _type
+        #``_type`` can be a string. This way you can reference a class
+        #that may not be defined yet. In this case we must explicity
+        #set type_name or an instance is raised inside ``__type_name``
+        if isinstance(_type, basestring):
+            type_name = _type
+        self.__type_name = type_name or self.__type_name(_type)
         
+    
     def validate(self, item):
-        if not isinstance(item, self._type):
-            raise ValidationError("Expected %s got %s instead." % (self._type.__name__, self._type_name(item)))
+        if not isinstance(item, self.__type):
+            raise ValidationError("Expected %s got %s instead." % (self.__type_name, self._class_name(item)))
         return item
-        
+    
+    def __type_name(self, _type):
+        if isinstance(_type, tuple):
+            return "(%s)" % ", ".join((t.__name__ for t in _type))
+        return _type.__name__
+    
     def __get__(self, obj, type=None):
 
         if type is None:
             return self
-        if not isinstance(self._type, basestring):
+        if not isinstance(self.__type, basestring):
             return self
-        #``_type`` can be a string. This way you can reference a class
-        #that may not be defined yet. But now we have to get the class
-        handler = _object_handlers.get(self._type)
-        print handler
+        #``_type`` was a string and now we must get the actual class
+        handler = _object_handlers.get(self.__type)
+
         if not handler:
-            raise JsonWebError("Cannot find class %s." % self._type)
+            raise JsonWebError("Cannot find class %s." % self.__type)
         return EnsureType(handler[1])
         
         
-class String(Validator):
-    def validate(self, item):
-        if not isinstance(item, basestring):
-            raise ValidationError("Expected string got %s instead." % self._type_name(item))
-        return item
+class String(EnsureType):
+    def __init__(self, **kw):
+        super(String, self).__init__(basestring, type_name="string", **kw)
 
-class Integer(Validator):
-    def validate(self, item):
-        if not isinstance(item, int):
-            raise ValidationError("Expected int got %s instead." % self._type_name(item))
-        return item         
+class Integer(EnsureType):
+    def __init__(self, **kw):
+        super(Integer, self).__init__(int, **kw)
     
-class Float(Validator):
-    def validate(self, item):
-        try:
-            return float(item)
-        except (TypeError, ValueError):
-            raise ValidationError("Expected float got %s instead." % self._type_name(item))        
-   
+class Float(EnsureType):
+    def __init__(self, **kw):
+        super(Float, self).__init__(float, **kw)
+        
+class Number(EnsureType):
+    def __init__(self, **kw):
+        super(Number, self).__init__((float, int), type_name="number", **kw)
