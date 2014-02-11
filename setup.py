cf6ddf7f6d2e50adbc439f0cc1d33429f3a63f76
Shawn Adams <boris317@gmail.com>
2014-02-11 13:03:40 -0500

    Updated long description in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+import os
 from setuptools import setup, find_packages
 
 version = '0.6.3'
 
+if os.path.exists("README.rst"):
+    long_description = open("README.rst").read()
+else:
+    long_description = "See http://www.jsonweb.net/en/latest"
+
 setup(name='JsonWeb',
       version=version,
       description="Package for building json de/serialization of your data models in a web app.",
-      long_description="""\
-""",
+      long_description=long_description,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Shawn Adams',
